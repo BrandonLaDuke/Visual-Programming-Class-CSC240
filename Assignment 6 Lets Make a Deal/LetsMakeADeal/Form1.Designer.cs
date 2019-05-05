0ca7e46330831d230091662dfb34ee26c74021e3
LADUKE <BLADUK8617@suscorp.edu>
2019-05-05 08:16:49 -0400

    Created initial commit of LetsMakeADeal Assignment

---
+++ Assignment 6 Lets Make a Deal/LetsMakeADeal/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LetsMakeADeal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPrompt = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.prizeLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelPrompt
+            // 
+            this.labelPrompt.AutoSize = true;
+            this.labelPrompt.Location = new System.Drawing.Point(13, 13);
+            this.labelPrompt.Name = "labelPrompt";
+            this.labelPrompt.Size = new System.Drawing.Size(117, 13);
+            this.labelPrompt.TabIndex = 0;
+            this.labelPrompt.Text = "Choose Door 1, 2, or 3.";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(16, 42);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(97, 42);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click_1);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(178, 42);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(13, 82);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 4;
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(16, 131);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(75, 23);
+            this.btnYes.TabIndex = 5;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click_1);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(97, 131);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 6;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click_1);
+            // 
+            // prizeLabel
+            // 
+            this.prizeLabel.AutoSize = true;
+            this.prizeLabel.Location = new System.Drawing.Point(13, 141);
+            this.prizeLabel.Name = "prizeLabel";
+            this.prizeLabel.Size = new System.Drawing.Size(0, 13);
+            this.prizeLabel.TabIndex = 7;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(16, 196);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(24, 13);
+            this.infoLabel.TabIndex = 8;
+            this.infoLabel.Text = "info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.prizeLabel);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.labelPrompt);
+            this.Name = "Form1";
+            this.Text = "Let\'s Make a Deal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPrompt;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label prizeLabel;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ Assignment 6 Lets Make a Deal/LetsMakeADeal/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetsMakeADeal
+{
+    public partial class Form1 : Form
+    {
+        //declair a random function, and a function
+        Random RandomClass = new Random();
+        int rand, door1, door2, door3, selectedDoor;
+        string[] prize = { "new car", "big-Screen TV", "Live Goat" }; // COntains the array of prizes.
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            this.ForeColor = Color.White;
+            btn1.ForeColor = Color.White;
+            btn1.BackColor = Color.Black;
+            btn2.ForeColor = Color.White;
+            btn2.BackColor = Color.Black;
+            btn3.ForeColor = Color.White;
+            btn3.BackColor = Color.Black;
+            btnYes.ForeColor = Color.White;
+            btnYes.BackColor = Color.Black;
+            btnNo.ForeColor = Color.White;
+            btnNo.BackColor = Color.Black;
+            infoLabel.ForeColor = Color.White;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = RandomClass.Next(0, 2); //Generates random number 0, 1
+            door1 = RandomClass.Next(0, 3);
+            door2 = RandomClass.Next(0, 3);
+            while (door1 == door2)
+            {
+                door2 = RandomClass.Next(0, 3);
+            }
+            if (door1 + door2 ==1)
+            {
+                door3 = 2;
+            } else if (door1 + door2 == 1)
+            {
+                door3 = 1;
+            } else
+            {
+                door3 = 0;
+            }
+            infoLabel.Text = "Door 1 is " + door1 + ", Door 2 is " + door2 + ", Door 3 is " + door3 + ".";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            selectedDoor = 0;
+            DisableButtons();
+            outputLabel.Text = "You chose Door 1";
+            if (door1 == 0)
+            {
+                if (rand == 0)
+                {
+                    outputLabel.Text += "\nDoor 2 Had a " + prize[door2];
+                }
+                else
+                {
+                    outputLabel.Text += "\nDoor 3 Had a " + prize[door3];
+                }
+            } else
+            {
+                if (door2 == 0)
+                {
+                    outputLabel.Text += "\nDoor3 had a " + prize[door3];
+                    rand = 1;
+                } else
+                {
+                    outputLabel.Text += "\nDoor 2 had a " + prize[door2];
+                    rand = 0;
+                }
+            }
+            outputLabel.Text += "\nDo you want to switch your selection?";
+            btnYes.Visible = true;
+            btnNo.Visible = true;
+        }
+        private void btn2_Click_1(object sender, EventArgs e)
+        {
+            selectedDoor = 0;
+            DisableButtons();
+            outputLabel.Text = "You chose Door 2";
+            if (door2 == 0)
+            {
+                if (rand == 0)
+                {
+                    outputLabel.Text += "\nDoor 1 Had a " + prize[door1];
+                }
+                else
+                {
+                    outputLabel.Text += "\nDoor 3 Had a " + prize[door3];
+                }
+            }
+            else
+            {
+                if (door1 == 0)
+                {
+                    outputLabel.Text += "\nDoor3 had a " + prize[door3];
+                    rand = 1;
+                }
+                else
+                {
+                    outputLabel.Text += "\nDoor 1 had a " + prize[door1];
+                    rand = 0;
+                }
+            }
+            outputLabel.Text += "\nDo you want to switch your selection?";
+            btnYes.Visible = true;
+            btnNo.Visible = true;
+        }
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            selectedDoor = 0;
+            DisableButtons();
+            outputLabel.Text = "You chose Door 3";
+            if (door3 == 0)
+            {
+                if (rand == 0)
+                {
+                    outputLabel.Text += "\nDoor 1 Had a " + prize[door1];
+                }
+                else
+                {
+                    outputLabel.Text += "\nDoor 2 Had a " + prize[door2];
+                }
+            }
+            else
+            {
+                if (door1 == 0)
+                {
+                    outputLabel.Text += "\nDoor2 had a " + prize[door2];
+                    rand = 1;
+                }
+                else
+                {
+                    outputLabel.Text += "\nDoor 1 had a " + prize[door1];
+                    rand = 0;
+                }
+            }
+            outputLabel.Text += "\nDo you want to switch your selection?";
+            btnYes.Visible = true;
+            btnNo.Visible = true;
+        }
+        private void btnYes_Click_1(object sender, EventArgs e)
+        {
+            if (selectedDoor == 0)
+            {
+                if (rand == 0)
+                {
+                    selectedDoor = 1;
+                } else
+                {
+                    selectedDoor = 2;
+                }
+            } else
+            {
+                if (selectedDoor == 1)
+                {
+                    if (rand == 0)
+                    {
+                        selectedDoor = 2;
+                    } else
+                    {
+                        selectedDoor = 0;
+                    }
+                } else
+                {
+                    if (rand == 0)
+                    {
+                        selectedDoor = 1;
+                    } else
+                    {
+                        selectedDoor = 0;
+                    }
+                }
+            }
+            RevealPrize();
+        }
+        private void btnNo_Click_1(object sender, EventArgs e)
+        {
+            RevealPrize();
+        }
+        private void RevealPrize()
+        {
+            btnYes.Enabled = false;
+            btnNo.Enabled = false;
+            if (selectedDoor == 0)
+            {
+                prizeLabel.Text = "You won a " + prize[door1];
+            } else if (selectedDoor == 1)
+            {
+                prizeLabel.Text = "You wan a " + prize[door2];
+            } else
+            {
+                prizeLabel.Text = "You won a " + prize[door3];
+            }
+        }
+        private void DisableButtons()
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+        }
+
+    }
+}
